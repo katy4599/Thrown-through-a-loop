2c9bb0d71740d843ea03114f03fadc8fa158d238
katy millard <katy.millard@gmail.com>
2022-02-08 14:37:59 -0600

    Open square

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const shopping = [
-    'Apples',
-    'Bananas',
-    'Pickles',
-    'Marshmallows',
-];
+function square(size) {
+    const char = '*';
+    for (let i = 0; i <= size; i += 1) {
+        let output = ' ';
 
-async function main () {
-    console.log('Here is your shopping list:');
-    console.log('===========================');
-    for (food of shopping) {
-        console.log(' = ' + food);
+        if (i == 0 || i == size) {
+            console.log(char.repeat(size));
+        } else {
+            let tab = output.repeat(size - 2);
+            console.log(char + tab + char);
+        } 
     }
+}
+async function main () {
+    square(9);
 }
 
 main();
