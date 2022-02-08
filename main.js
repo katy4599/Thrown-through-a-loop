2965e35d057a83b1cbffae67ae33fa8f2648394b
katy millard <katy.millard@gmail.com>
2022-02-08 11:37:37 -0600

    for loop square

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
 import input from 'input';
 
-function say(i) {
-    switch(i) {
-        case '1':
-            console.log('one');  
-            break;  
-        case '2':
-            console.log('two'); 
-            break;
-        case '3':
-            console.log('three');  
-            break;   
-        case '4':
-            console.log('four'); 
-            break;  
-        case '5':
-            console.log('five');  
-            break; 
-        case '6':
-            console.log('six');
-            break;    
-        case '7':
-            console.log('seven');
-            break;
-        case '8':
-            console.log('eight'); 
-            break;     
-        case '9':
-            console.log('nine');
-            break;   
-        case '10':
-            console.log('ten');  
-            break;
-    }
+function square(size) {
+    for (let i = 1; i <= size; i += 1) {
+        console.log('X'.repeat(size));
+    } 
 }
 
-
 async function main() {
-    for (let i = 1; i <= 10; i += 1) {
-        say((i).toString());
-    }
+    const size = await input.text('Enter a size.'); 
+    square(size);
 }
 
 main();
